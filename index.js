58955fdea9c28d9445be06351cc28ad89c3ab7ab
Michelle <nyamburamichelle400@gmail.com>
2024-03-23 00:04:47 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+
+
+var customerName ="bob"
+ function upperCaseCustomerName(){
+     customerName =customerName.toUpperCase()
+     return customerName
+ }
+ function setBestCustomer() {
+    //  let bestCustomer ="not bob"
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+ function overwriteBestCustomer() {
+    bestCustomer= "maybe bob"
+     return bestCustomer
+} 
+ 
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = 'Anna'
+   leastFavoriteCustomer = "Marrie"
+   return leastFavoriteCustomer
+}
+
+
